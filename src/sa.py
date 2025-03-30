874959503ed42cc2ff007906dde1753f360e0ef9
maron8676 <maron8676@gmail.com>
2025-03-30 23:45:56 +0900

    焼いてみたが、意味なさそうなので中止

--- src/sa.py
+++ src/sa.py
@@ -1,9 +1,14 @@
+from __future__ import annotations
+
 import math
+import random
 import sys
+import time
 from collections import defaultdict
 from sys import stdin
 
 readline = stdin.readline
+start_time = time.time()
 
 
 class City:
@@ -19,6 +24,33 @@ class City:
 
     def mean(self):
         return [(self.lx + self.rx) // 2, (self.ly + self.ry) // 2]
+
+    def get_bbox(self) -> BBox:
+        return BBox(self.lx, self.rx, self.ly, self.ry)
+
+
+class BBox:
+    def __init__(self, lx, rx, ly, ry):
+        self.lx = lx
+        self.rx = rx
+        self.ly = ly
+        self.ry = ry
+
+    def intersection(self, other_bbox: BBox) -> BBox:
+        x_overlap = [max(self.lx, other_bbox.lx), min(self.rx, other_bbox.rx)]
+        y_overlap = [max(self.ly, other_bbox.ly), min(self.ry, other_bbox.ry)]
+        return BBox(*x_overlap, *y_overlap)
+
+    def is_collision(self, other_bbox: BBox) -> bool:
+        x_overlap = max(self.lx, other_bbox.lx) < min(self.rx, other_bbox.rx)
+        y_overlap = max(self.ly, other_bbox.ly) < min(self.ry, other_bbox.ry)
+        return x_overlap and y_overlap
+
+    def union(self, other_bbox: BBox):
+        self.lx = min(self.lx, other_bbox.lx)
+        self.rx = max(self.rx, other_bbox.rx)
+        self.ly = min(self.ly, other_bbox.ly)
+        self.ry = max(self.ry, other_bbox.ry)
 
 
 class UnionFind():
@@ -95,10 +127,35 @@ def answer(groups, edges):
             print(*e)
 
 
-def calc_dis(city1: City, city2: City) -> float:
+def calc_dis(city1: City, city2: City) -> int:
     pos1 = city1.mean()
     pos2 = city2.mean()
-    return math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2)
+    return math.floor(math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2))
+
+
+def calc_bbox(group: list[City]):
+    assert len(group) > 0, "bbox error: empty city list"
+    result = group[0].get_bbox()
+    for i in range(1, len(group)):
+        other_bbox = group[i].get_bbox()
+        result.union(other_bbox)
+    return result
+
+
+def calc_cost(city_list: list[City], edges: list[list[int]]) -> int:
+    cost = 0
+
+    city_dict = dict()
+    for city in city_list:
+        city_dict[city.index] = city
+
+    for edge in edges:
+        u = city_dict[edge[0]]
+        v = city_dict[edge[1]]
+        dis = calc_dis(u, v)
+        cost += dis
+
+    return cost
 
 
 N, M, Q, L, W = li()
@@ -143,16 +200,14 @@ for g in G:
 # get edges from queries
 edges = []
 
-# 位置が正しいと仮定して解析的に辺を作成する
-for group in groups:
-    edge = []
 
-    edge_candidate_list = []
+def cons_minimum_tree(group: list[City]):
+    edge = []
+    edge_candidate_list: list[tuple[float, int, int]] = []
     for i in range(len(group)):
         for j in range(i + 1, len(group)):
             edge_candidate_list.append((calc_dis(group[i], group[j]), i, j))
     edge_candidate_list.sort(key=lambda x: x[0])
-
     index_map = {i: v.index for i, v in enumerate(group)}
     uf = UnionFind(len(group))
     for edge_candidate in edge_candidate_list:
@@ -164,9 +219,76 @@ for group in groups:
         if uf.group_count() == 1:
             break
 
+    return edge
+
+
+# 位置が正しいと仮定して解析的に辺を作成する
+for group in groups:
+    edge = cons_minimum_tree(group)
     edges.append(edge)
 
 # 1秒焼く
+sa_count = 0
+T = 1.0
+while time.time() - start_time < 1.0:
+    sa_count += 1
+
+    source_index = random.randint(0, len(groups) - 1)
+    source_group = groups[source_index]
+
+    source_bbox = calc_bbox(source_group)
+
+    target_index = random.randint(0, len(groups) - 1)
+    if source_index == target_index:
+        continue
+
+    target_group = groups[target_index]
+    target_bbox = calc_bbox(groups[target_index])
+    if not source_bbox.is_collision(target_bbox):
+        continue
+
+    # intersection = source_bbox.intersection(target_bbox)
+
+    # source_indices = [index for index, city in enumerate(source_group)
+    #                   if city.get_bbox().is_collision(intersection)]
+    # target_indices = [index for index, city in enumerate(target_group)
+    #                   if city.get_bbox().is_collision(intersection)]
+    #
+    # if len(source_indices) == 0 or len(target_indices) == 0:
+    #     continue
+
+    before_source_cost = calc_cost(source_group, edges[source_index])
+    before_target_cost = calc_cost(target_group, edges[target_index])
+    before_cost = before_source_cost + before_target_cost
+
+    # swap
+    s1 = random.choice(list(range(len(source_group))))
+    s2 = random.choice(list(range(len(target_group))))
+
+    temp = source_group.pop(s1)
+    source_group.append(target_group.pop(s2))
+    target_group.append(temp)
+
+    # 最小全域木を再計算して、辺の長さの和を比較する
+    after_source_edges = cons_minimum_tree(source_group)
+    after_target_edges = cons_minimum_tree(target_group)
+    after_source_cost = calc_cost(source_group, after_source_edges)
+    after_target_cost = calc_cost(target_group, after_target_edges)
+    after_cost = after_source_cost + after_target_cost
+
+    # 採用するか判定
+    # if random.random() < math.exp(max(min((before_cost - after_cost) / max(float(T), 0.0001), 1), -20)):
+    if before_cost - after_cost > 0:
+        edges[source_index] = after_source_edges
+        edges[target_index] = after_target_edges
+    else:
+        temp = source_group.pop(len(source_group) - 1)
+        source_group.append(target_group.pop(len(target_group) - 1))
+        target_group.append(temp)
+
+    T -= 0.0001
 
 # output answer
 answer(groups, edges)
+
+print(sa_count, time.time() - start_time, file=sys.stderr)
